01981fe1e50b8f483d863ee2c3edfa7eaff1e3c2
Xavier Brassard <xtartaglia@gmail.com>
2022-01-17 11:40:24 +0100

    lol

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var app = require('express')(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server),
     ent = require('ent');
+ 
+app.set('port', (process.env.PORT || 4000));
 
 app.get('/',(req, res)=>{
     res.sendFile(__dirname + '/index.html');
@@ -19,7 +21,7 @@ app.get('/img/retro.jpg', (req, res)=>{
     res.sendFile(__dirname + '/img/retro.jpg');
 })
 
-server.listen(8080,()=>{
+server.listen(port,()=>{
     console.log('listening on 8080');
 });
 
